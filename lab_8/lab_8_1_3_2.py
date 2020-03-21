d4e5902cc62417a5d5597c03e08e94486ae895f0
Eger37 <60922554+Eger37@users.noreply.github.com>
2020-03-21 15:27:57 +0200

    Add files via upload

--- lab_8/lab_8_1_3_2.py
+++ lab_8/lab_8_1_3_2.py
@@ -25,103 +25,144 @@ def my_input(text_def):
     return input_num
 
 
-def line_search(last_def, g_def, x_def, tr_def=False):
-    a_def = np.random.randint(0, last_def, g_def)
-    if tr_def:
-        a_def[g_def // 2] = x_def
-    print("random number array :")
+def line_search(a_def, x_def):
+    print("array :")
     print(a_def)
     i_def = 0
-    while i_def < g_def:
-        if a_def[i_def] == x_def:
-            i_def += 1
-            break
+    g_def = len(a_def)
+    counter_def = 0
+    while i_def < g_def and a_def[i_def] != x_def:
+        counter_def += 2
         i_def += 1
     if i_def == g_def:
         print('Item', x_def, 'not found.')
-        print('number of comparisons:', i_def)
+        print('number of comparisons:', counter_def)
     else:
         print('Item', x_def, 'first found in position', i_def)
-        print('number of comparisons:', i_def)
+        print('number of comparisons:', counter_def)
 
 
-def binary_search(last_def, g_def, x_def, tr_def=False):
-    a_def = np.random.randint(0, last_def, g_def)
-    if tr_def:
-        a_def[g_def // 2] = x_def
-    a_def.sort()
-    print("random number array :")
+def binary_search(a_def, x_def):
+    a_def.sort()# сортировка массива
+    g_def = len(a_def)
+    print("array :")
     print(a_def)
     i = 0
     L = 0
     R = g_def - 1
     flag = True
+    counter_def = 0
     while L <= R and flag:
         i += 1
         K = (L + R) // 2
         if a_def[K] == x_def:
+            counter_def += 2
             print('Item', x_def, 'first found in position', K)
-            print('number of comparisons:', i)
+            print('number of comparisons:', counter_def)
             flag = False
         elif a_def[K] < x_def:
+            counter_def += 3
             L = K + 1
         elif a_def[K] > x_def:
+            counter_def += 4
             R = K - 1
     if flag:
         print('Item', x_def, 'not found.')
-        print('number of comparisons:', i)
+        print('number of comparisons:', counter_def)
 
 
 def patter_search(text_def, pattern_def):
     i = -1
     j = 0
-    count_def = 0
+    counter_def = 0
     while (j < len(pattern_def)) and i < (len(text_def) - len(pattern_def)):
+        counter_def += 2
         j = 0
         i += 1
         while j < len(pattern_def) and pattern_def[j] == text_def[i + j]:
+            counter_def += 2
             j += 1
-            count_def += 1
     if j == len(pattern_def):
         print('Pattern', pattern_def, 'first found in position', i)
     else:
         print('Item', pattern_def, 'not found.')
-    print('number of comparisons:', count_def)
+    print('number of comparisons:', counter_def)
 
 
 while True:
     print(" лінійний пошук:\n")
-    # # g = my_input('count of elements: ')
-    # # x = my_input('required element (int 0-100): ')
-
-    g = 20
-    x = 15
-    last = 15
-    line_search(last, g, x)
-    print()
-    g = 20
-    x = 2
-    last = 10
-    line_search(last, g, x, True)
+    quesh = input(
+        "Введите 'yes', если хотите ввести данные самостоятельно, в противном случае будет использоваться программная генерация: ")
+    if quesh == 'yes':
+        g = my_input('count of elements: ')  # длина массива
+        x = my_input('required element: ')  # искомый элемент
+        a = np.zeros(g, dtype=int)
+        print("Ввод массива:")
+        for i in range(g):
+            a[i] = my_input("Введите целое число: ")
+        line_search(a, x)  # вызов функции линейного поиска
+    # g = my_input('count of elements: ')
+    # x = my_input('required element (int 0-100): ')
+    else:
+        # Искомый элемент не присутсвует в массиве
+        g = 20  # длина массива
+        x = 15  # искомый элемент
+        last = 15
+        a = np.random.randint(0, last, g)  # (start, stop, len) генерация рандомного массива
+        line_search(a, x)  # вызов функции линейного поиска
+        print()
+        # Искомый элемент присутсвует в массиве
+        g = 20  # длина массива
+        x = 2  # искомый элемент
+        last = 10
+        a = np.random.randint(0, last, g)  # (start, stop, len) генерация рандомного массива
+        a[-1] = x
+        line_search(a, x)  # вызов функции линейного поиска
 
     print("\n бінарний пошук:\n")
-    # А[i - 1] <= А[i], для 1 <= i <= (N – 1),
-    g = 20
-    x = 12
-    last = 10
-    binary_search(last, g, x, False)
-    print()
-    g = 20
-    x = 2
-    last = 10
-    binary_search(last, g, x, True)
+    quesh = input(
+        "Введите 'yes', если хотите ввести данные самостоятельно, в противном случае будет использоваться программная генерация: ")
+    if quesh == 'yes':
+        g = my_input('count of elements: ')  # длина массива
+        x = my_input('required element: ')  # искомый элемент
+        a = np.zeros(g, dtype=int)
+        print("Ввод массива:")
+        for i in range(g):
+            a[i] = my_input("Введите целое число: ")
+        binary_search(a, x)# вызов функции бинарного поиска
+    # g = my_input('count of elements: ')
+    # x = my_input('required element (int 0-100): ')
+    else:
+        g = 20
+        x = 12
+        last = 10
+        a = np.random.randint(0, last, g)  # (start, stop, len) генерация рандомного массива
+        binary_search(a, x)# вызов функции бинарного поиска
+        print()
+        g = 20
+        x = 2
+        last = 10
+        a = np.random.randint(0, last, g)  # (start, stop, len) генерация рандомного массива
+        binary_search(a, x)# вызов функции бинарного поиска
+
+
+
     print("\n прямий пошук підрядка:\n")
-    text = "мама мыла раму"
-    pattern = "раму"
-    patter_search(text, pattern)
-    print()
-    pattern = "мамка"
-    patter_search(text, pattern)
+    quesh = input(
+        "Введите 'yes', если хотите ввести данные самостоятельно, в противном случае будет использоваться программная генерация: \n")
+    if quesh == 'yes':
+        text = input("Введите строку")
+        pattern = input("Введите подстроку, которую нужно найти")
+        patter_search(text, pattern)
+    else:
+        # Искомая подстрока присутсвует в строке
+        text = "мама мыла раму"
+        pattern = "раму"
+        patter_search(text, pattern)
+        print()
+        # Искомая подстрока не присутсвует в строке
+        pattern = "мамка"
+        patter_search(text, pattern)
     if input('Нажмите "Enter" (введите пустую строку (\'\')) для перезапуска: ') == '':
         continue
     break
